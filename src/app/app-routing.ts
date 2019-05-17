2f2cdbb120f7ee957fec293fa0aff25fda040e86
komal bhale <komalbhale007@gmail.com>
2019-05-17 23:33:18 +0530

    Error fixed.

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SideNavComponent } from './side-nav/side-nav.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
